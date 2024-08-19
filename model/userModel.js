438ed4b2ddaef98d31baab59da935f9f29c57a01
Muhammad Hassan <k214577@nu.edu.pk>
2024-08-19 18:55:33 +0500

    comparePassword added in userModel

--- model/userModel.js
+++ model/userModel.js
@@ -1,30 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: [true, "Username required"]
-        },
-        email: {
-            type: String,  
-            required: [true, "Email required"],
-            unique: true,  
-            lowercase: true,  
-            match: [/\S+@\S+\.\S+/, 'Please enter a valid email address']
-        },
-        password: {
-            type: String,
-            required: [true, "Password required"]
-        },
-        role: {
-            type: String,
-            enum: ['user', 'admin'],
-            default: 'user'
-        },
-
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username required"]
     },
-    { timestamps: true }
-);
+    email: {
+        type: String,
+        required: [true, "Email required"],
+        unique: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Please enter a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, "Password required"]
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+}, { timestamps: true });
+
+userSchema.methods.comparePassword = function(enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
